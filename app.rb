39969d0197dcc12387b90b8c71c7cfa1c5a0dfd8
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2020-12-07 15:46:00 +0000

    first commit

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
+
+get '/' do
+  "Hi"
+end
+
+get '/secret' do
+  "This is super super secret indeed."
+end
+
+get '/ollie' do
+  "Ollie is my pair partner today."
+end
