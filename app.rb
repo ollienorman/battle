c5dd6b015e0462fa1df83924f072cd797d73218a
AJ <aj.monty@gmail.com>
2020-12-09 16:18:21 +0000

    added player 1 attack

--- app.rb
+++ app.rb
@@ -1,7 +1,6 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-
   enable :sessions
 
   get '/' do
@@ -11,14 +10,22 @@ class Battle < Sinatra::Base
   post '/names' do
     session[:player1] = params[:Player1]
     session[:player2] = params[:Player2]
+    session[:player1hp] = 10
+    session[:player2hp] = 10
     redirect '/play'
   end
 
   get '/play' do
     @player1 = session[:player1]
     @player2 = session[:player2]
+    @player1hp = session[:player1hp]
+    @player2hp = session[:player2hp]
     erb(:play)
   end
 
-  run! if app_file == $0
+  get '/player_1_attack' do
+    session[:player2hp] -= 1
+    redirect '/play'
+  end
+  run! if app_file == $PROGRAM_NAME
 end
